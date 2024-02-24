9fc49cce22aa82e0e11e280e04a5af04ca1cd88b
JuanchiTaveira <juantaveira10@gmail.com>
2024-02-24 15:22:02 +0100

    get categories from API call

--- app/src/main/java/com/example/evaluablet2/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/evaluablet2/ui/activities/MainActivity.kt
@@ -5,13 +5,21 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Request.Method
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.evaluablet2.Product
 import com.example.evaluablet2.adapter.ProductAdapter
 import com.example.evaluablet2.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -47,9 +55,34 @@ class MainActivity : AppCompatActivity() {
                 Snackbar.make(binding.root, category, Snackbar.LENGTH_SHORT).show()
             }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-            }
-
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
         }
+
+        fillCategorySpinner()
+    }
+
+    fun fillCategorySpinner() {
+        val categories = ArrayList<String>()
+
+        val jsonArrayRequest = JsonArrayRequest(
+            Request.Method.GET,
+            "https://dummyjson.com/products/categories",
+            null,
+            Response.Listener<JSONArray> { response ->
+                for (i in 0 until response.length()) {
+                    categories.add(response.getString(i))
+                }
+
+                val adapter = ArrayAdapter(
+                    this,
+                    android.R.layout.simple_spinner_item,
+                    categories
+                )
+                binding.spinnerCategories.adapter = adapter
+            },
+            null
+        )
+
+        Volley.newRequestQueue(applicationContext).add(jsonArrayRequest)
     }
 }
