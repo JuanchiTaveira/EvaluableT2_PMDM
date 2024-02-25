e8585901fbead5a7c3780dc9e7072dc98634b212
JuanchiTaveira <juantaveira10@gmail.com>
2024-02-25 14:29:05 +0100

    SecondActivity working

---
+++ app/src/main/java/com/example/evaluablet2/adapter/CartAdapter.kt
@@ -0,0 +1,64 @@
+package com.example.evaluablet2.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.example.evaluablet2.Product
+import com.example.evaluablet2.R
+
+class CartAdapter(var products: ArrayList<Product>, var context: Context) : RecyclerView.Adapter<CartAdapter.MyHolder>() {
+
+    private lateinit var listener: OnRemoveFromCartListener
+
+    init {
+        listener = context as OnRemoveFromCartListener
+    }
+
+    class MyHolder(itemView: View) : ViewHolder(itemView) {
+        var image: ImageView
+        var title: TextView
+        var price: TextView
+        var btnRemove: Button
+
+        init {
+            image = itemView.findViewById(R.id.cart_row_image)
+            title = itemView.findViewById(R.id.cart_row_title)
+            price = itemView.findViewById(R.id.cart_row_price)
+            btnRemove = itemView.findViewById(R.id.btn_remove_from_cart)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.cart_item_recycler, parent, false)
+        return MyHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return products.size
+    }
+
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+        val product = products.get(position)
+
+        Glide.with(context).load(product.images[0]).into(holder.image)
+        holder.title.text = product.title
+        holder.price.text = product.price.toString() + " EUR"
+
+        holder.btnRemove.setOnClickListener {
+            listener.removeFromCart(product)
+            notifyItemRemoved(position)
+        }
+    }
+
+    interface OnRemoveFromCartListener {
+        fun removeFromCart(product: Product)
+    }
+
+}
--- app/src/main/java/com/example/evaluablet2/ui/activities/SecondActivity.kt
+++ app/src/main/java/com/example/evaluablet2/ui/activities/SecondActivity.kt
@@ -2,19 +2,39 @@ package com.example.evaluablet2.ui.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.evaluablet2.Product
+import com.example.evaluablet2.adapter.CartAdapter
 import com.example.evaluablet2.databinding.ActivitySecondBinding
 
-class SecondActivity: AppCompatActivity() {
+class SecondActivity: AppCompatActivity(), CartAdapter.OnRemoveFromCartListener {
 
     private lateinit var binding: ActivitySecondBinding
     private lateinit var cart: ArrayList<Product>
+    private lateinit var cartAdapter: CartAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivitySecondBinding.inflate(layoutInflater)
-        cart = intent.getSerializableExtra("cartProducts") as ArrayList<Product>
+        setContentView(binding.root)
 
+        cart = intent.getSerializableExtra("cartProducts") as ArrayList<Product>
+        binding.tvTotal.text = "Total: " + getTotal()
+
+        cartAdapter = CartAdapter(cart, this)
+        binding.reciclerProducts.adapter = cartAdapter
+        binding.reciclerProducts.layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+
+    }
+
+    override fun removeFromCart(product: Product) {
+        cart.remove(product)
+        binding.tvTotal.text = "Total: " + getTotal()
+    }
+
+    private fun getTotal(): Int {
+        var total = 0
+        cart.forEach { product -> total += product.price }
+        return total
     }
 }
